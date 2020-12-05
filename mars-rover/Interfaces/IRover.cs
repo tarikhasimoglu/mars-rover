3db479d036f70cc6a355429bc1987627054d61fb
Tarık Haşimoğlu <tarik.hasimoglu@advancity.com.tr>
2020-12-05 21:20:59 +0300

    Initial Rover implementation

---
+++ mars-rover/Interfaces/IRover.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mars_rover.Interfaces
+{
+    public interface IRover
+    {
+        public int RoverX { get; set; }
+        public int RoverY { get; set; }
+    }
+}
---
+++ mars-rover/Rover.cs
@@ -0,0 +1,19 @@
+﻿using mars_rover.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mars_rover
+{
+    class Rover : IRover
+    {
+        public int RoverX { get; set; }
+        public int RoverY { get; set; }
+
+        public Rover(int RoverX, int RoverY)
+        {
+            this.RoverX = RoverX;
+            this.RoverY = RoverY;
+        }
+    }
+}
