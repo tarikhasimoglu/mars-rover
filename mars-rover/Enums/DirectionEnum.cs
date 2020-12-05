0aa37e54f89aa53c282d5b7a56fa97f30babe32b
Tarık Haşimoğlu <tarik.hasimoglu@advancity.com.tr>
2020-12-05 21:19:33 +0300

    Add DirectionEnum

---
+++ mars-rover/Enums/DirectionEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace mars_rover.Enums
+{
+    public enum DirectionEnum
+    {
+        [Description("N")]
+        North,
+        [Description("E")]
+        East,
+        [Description("S")]
+        South,
+        [Description("W")]
+        West
+    }
+}
