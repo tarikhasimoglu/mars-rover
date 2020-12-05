420478a92b5b8ab81a309d9b1b0626ee258f4218
Tarık Haşimoğlu <tarik.hasimoglu@advancity.com.tr>
2020-12-05 23:14:26 +0300

    Add Command Enum

---
+++ mars-rover/Enums/Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mars_rover.Enums
+{
+    public enum Commands
+    {
+        ///<summary>Look Right</summary>
+        R = 0,
+        ///<summary>Look Left</summary>
+        L = 1,
+        ///<summary>Move</summary>
+        M = 2,
+    }
+}
